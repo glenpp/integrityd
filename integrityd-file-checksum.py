03850a5e622bd8f0cfe370997497775f35859bb7
Glen Pit-Pladdy <glenpp@users.noreply.github.com>
2016-07-11 08:34:02 +0100

    Licensing headers added

--- integrityd-file-checksum.py
+++ integrityd-file-checksum.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3.4
+#    Log Anomaly monitoring and reporting daemon - checksum helper
+#    Copyright (C) 2011,2016  Glen Pitt-Pladdy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# For effective checksumming (eg. avoid impacting cache we need Python 3.3+
+# Since many needed modules are not widely distributed beyond 2.7 yet this
+# part has been separated to allow capabilities in 3.3+ to be used.
+#
+# See: https://www.pitt-pladdy.com/blog/
+#
+
+
 # takes a path on STDIN
 # outputs SHA1 (or NULL) and path on STDOUT
 
--- integrityd-log.py
+++ integrityd-log.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
+#    Log Anomaly monitoring and reporting daemon
+#    Copyright (C) 2011,2016  Glen Pitt-Pladdy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# This is a redesigned version of a combined File & Log integrity tool
+# written in Perl. This has been fully redesigned based on the original
+# concepts for the Perl version.
+#
+# See: https://www.pitt-pladdy.com/blog/
+#
 
 
 import sys
